b9961364fc3703a4a97f966db672abe5b1a5777a
SherineAntony <sherineak@gmail.com>
2024-08-23 13:39:27 +0530

    Api Testing

---
+++ src/test/java/org/example/ex_1_restAssured/Lab260.java
@@ -0,0 +1,18 @@
+package org.example.ex_1_restAssured;
+
+import io.restassured.RestAssured;
+
+public class Lab260 {
+    public static void main(String[] args) {
+        //Rest Assured Hello World program
+        // Sample example for APi Automate using Given_When _then
+
+        RestAssured
+                .given().baseUri("https://restful-booker.herokuapp.com").basePath("/ping")
+                .when().get()
+                .then().statusCode(201);
+
+
+
+    }
+}
